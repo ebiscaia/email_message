3c884c431ed9e63ffea336e7ebc49619e24bc9b5
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2026-01-21 18:56:26 +1100

    Changed print statement to verify file contents

--- email.py
+++ email.py
@@ -6,7 +6,7 @@ AUTH_FILE = "auth.json"
 with open(PHRASE_FILE) as f:
     lines = f.read().splitlines()
 
-print(len(lines))
 with open(AUTH_FILE) as af:
     auth = json.load(af)
 
+print(auth["service"])
