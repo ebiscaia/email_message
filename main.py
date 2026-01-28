b974d0fed87d549a34aa0fb45c4fed674756acec
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2026-01-28 19:29:39 +1100

    Moved code block to after phrase manipulation block

--- main.py
+++ main.py
@@ -13,19 +13,20 @@ with open(PHRASE_FILE) as f:
 with open(AUTH_FILE) as af:
     auth = json.load(af)
 
-# try sending an email
-msg = EmailMessage()
-msg["Subject"] = "Test Email"
-msg["From"] = auth["email"]
-msg["To"] = auth["to"]
-msg.set_content("This is a test email sent via Python.")
-
 # Associate the lines of the PHRASE_FILE to variables of languages and phrases
 languages = lines[0].split("|")
 phrases = lines[1:]
 
 # Get a random phrase and separate the original part and its translation
 phrase = phrases[random.randint(0, len(phrases) - 1)].split("|")
+
+
+# try sending an email
+msg = EmailMessage()
+msg["Subject"] = "Test Email"
+msg["From"] = auth["email"]
+msg["To"] = auth["to"]
+msg.set_content(message)
 
 # Send the email securely (works with iCloud and Gmail):
 with smtplib.SMTP(auth["outgoing_server"], auth["outgoing_port"]) as server:
