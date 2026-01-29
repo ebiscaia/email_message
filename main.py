3a4f4ae4c155a54461c5b1d66b6cad375a969902
Eduardo Biscaia de Queiroz <ebiscaia@me.com>
2026-01-29 17:33:22 +1100

    Modified file to allow error handling when opening files

--- main.py
+++ main.py
@@ -8,50 +8,54 @@ PHRASE_FILE = "phrases.txt"
 AUTH_FILE = "auth.json"
 message = ""
 
-with open(PHRASE_FILE) as f:
-    lines = f.read().splitlines()
+try:
+    with open(PHRASE_FILE) as f, open(AUTH_FILE) as af:
+        lines = f.read().splitlines()
+        auth = json.load(af)
+except Exception as e:
+    print(f"Error: {e}")
 
-with open(AUTH_FILE) as af:
-    auth = json.load(af)
+else:
+    # Associate the lines of the PHRASE_FILE to variables of languages and phrases
+    languages = lines[0].split("|")
+    phrases = lines[1:]
 
-# Associate the lines of the PHRASE_FILE to variables of languages and phrases
-languages = lines[0].split("|")
-phrases = lines[1:]
+    # Get a random phrase and separate the original part and its translation
+    index = random.randint(0, len(phrases) - 1)
+    phrase = phrases[index].split("|")
 
-# Get a random phrase and separate the original part and its translation
-index = random.randint(0, len(phrases) - 1)
-phrase = phrases[index].split("|")
+    # Build the message
+    message = f"""Phrase of the day
+    {languages[0]}: {phrase[0]}
+    {languages[1]}: {phrase[1]}"""
 
-# Build the message
-message = f"""Phrase of the day
-{languages[0]}: {phrase[0]}
-{languages[1]}: {phrase[1]}"""
+    # Check the lenght of phrases array and add a warning to the main message
+    if len(phrases) == 1:
+        message += """
+     
+    This is the last phrase of the file. Generate a new version of the file,
+    otherwise this program will fail next time.
+    """
 
-# Check the lenght of phrases array and add a warning to the main message
-if len(phrases) == 1:
-    message += """
- 
-This is the last phrase of the file. Generate a new version of the file,
-otherwise this program will fail next time.
-"""
+        # try sending an email
+        msg = EmailMessage()
+        msg["Subject"] = "Test Email"
+        msg["From"] = auth["email"]
+        msg["To"] = auth["to"]
+        msg.set_content(message)
 
-# try sending an email
-msg = EmailMessage()
-msg["Subject"] = "Test Email"
-msg["From"] = auth["email"]
-msg["To"] = auth["to"]
-msg.set_content(message)
+        # Send the email securely (works with iCloud and Gmail):
+        with smtplib.SMTP(
+            auth["outgoing_server"], auth["outgoing_port"]
+        ) as server:
+            server.starttls()
+            server.login(auth["email"], auth["password"])
+            server.send_message(msg)
 
-# Send the email securely (works with iCloud and Gmail):
-with smtplib.SMTP(auth["outgoing_server"], auth["outgoing_port"]) as server:
-    server.starttls()
-    server.login(auth["email"], auth["password"])
-    server.send_message(msg)
+    # Update lines array without the sent phrase (index+1 because of header)
+    lines.pop(index + 1)
+    linesString = "\n".join(lines)
 
-# Update lines array without the sent phrase (index+1 because of header)
-lines.pop(index + 1)
-linesString = "\n".join(lines)
-
-# Overwrite the file
-with open("phrases.txt", "w") as f:
-    f.write(linesString)
+    # Overwrite the file
+    with open("phrases.txt", "w") as f:
+        f.write(linesString)
