4dc7822fbb236154fb4e0cca46abee4a5f513d93
Eduardo Biscaia de Queiroz <eddie@localhost.localdomain>
2026-02-09 19:50:23 +1100

    final changes to main program and some file rearrangements.

--- main.py
+++ main.py
@@ -4,34 +4,59 @@ import logging
 import random
 
 from email.message import EmailMessage
+from logging.handlers import TimedRotatingFileHandler
 
-# Set the logging system up
-logging.basicConfig(
-    level=logging.DEBUG,
+# Define some of the main variables
+PHRASE_FILE = "files/phrases.txt"
+AUTH_FILE = "files/auth.json"
+LOG_FILE = "files/app.log"
+message = ""
+
+# Create a logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create a file handler
+file_handler = TimedRotatingFileHandler(
+    filename=LOG_FILE, when="W6", backupCount=10, encoding="utf-8"
+)
+
+# Create a console handler
+console_handler = logging.StreamHandler()
+
+# Set the handler formatter
+formatter = logging.Formatter(
     datefmt="%d/%m/%Y %H:%M:%S",
-    format="{asctime}:{levelname}:{message}",
+    fmt="{name}:{asctime}:{levelname}: {message}",
     style="{",
 )
 
-PHRASE_FILE = "phrases.txt"
-AUTH_FILE = "auth.json"
-message = ""
+# Set the formatter to the handlers
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
 
+# Add the handler to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+# Main program
 try:
     with open(AUTH_FILE) as af, open(PHRASE_FILE) as f:
         auth = json.load(af)
         lines = f.read().splitlines()
 
     for file in [AUTH_FILE, PHRASE_FILE]:
-        logging.debug(f"File {file} read.")
+        logger.debug(f"File {file} read.")
 
     # Associate the lines of the PHRASE_FILE to variables of languages and phrases
     languages = lines[0].split("|")
     phrases = lines[1:]
 
+    logger.debug(f"Language pair: {languages[0]}/{languages[1]}")
+
     # Check whether there are any phrases to be sent
     if not phrases:
-        print("There are no phrases to be sent. Stopping program.")
+        logger.error("There are no phrases to be sent. Stopping program.")
         exit(1)
 
     # Get a random phrase and separate the original part and its translation
@@ -39,7 +64,7 @@ try:
     phrase = phrases[index].split("|")
 
     # Build the message
-    message = f"""Phrase of the day
+    message = f"""
     {languages[0]}: {phrase[0]}
     {languages[1]}: {phrase[1]}"""
 
@@ -50,10 +75,18 @@ try:
     This is the last phrase of the file. Generate a new version of the file,
     otherwise this program will fail next time.
     """
+        # Also log it
+        logger.warning(
+            "Last remaining phrase. Update the file to avoid an error next time the program is run."
+        )
+    else:
+        logger.info(
+            f"There is/are {len(phrases)-1} remaining phrase(s) in the file."
+        )
 
     # try sending an email
     msg = EmailMessage()
-    msg["Subject"] = "Test Email"
+    msg["Subject"] = "Phrase of the day"
     msg["From"] = auth["email"]
     msg["To"] = auth["to"]
     msg.set_content(message)
@@ -67,8 +100,10 @@ try:
     lines.pop(index + 1)
     linesString = "\n".join(lines)
 
+    logger.debug(f'An email was sent to {auth["to"]} successfully.')
     # Overwrite the file
-    with open("phrases.txt", "w") as f:
+    with open(PHRASE_FILE, "w") as f:
         f.write(linesString)
+    logger.debug(f"File {PHRASE_FILE} was saved successfully.")
 except Exception as e:
-    logging.error(f"Error: {e}.")
+    logger.error(f"Error: {e}.")
